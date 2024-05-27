215906cc48a4aa2765c18bac69a4afef17f1ef8e
Dominionsys-Inc <mbinuintangku@gmail.com>
2024-05-27 17:15:50 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">=0.12.0"
   backend "s3" {
-    key     = "terraformstatefile"
+    key     = "terraformstate"
     bucket = "teamabucket"
     region = "us-west-2"
     #dynamodb_table = "terraform-state-locking"
