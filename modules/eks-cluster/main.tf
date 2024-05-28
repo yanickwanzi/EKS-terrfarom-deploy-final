fb8b491ed2c10f06febed42a59793e3d2b60aac7
Dominionsys-Inc <mbinuintangku@gmail.com>
2024-05-27 20:00:59 -0500

    Update main.tf

--- modules/eks-cluster/main.tf
+++ modules/eks-cluster/main.tf
@@ -65,8 +65,8 @@ module "eks" {
   aws_auth_roles = [
     {
       rolearn  = var.rolearn
-      username = "fusi"
-      groups   = ["system:masters"]
+      username = "TeamA-User"
+      groups   = ["TeamA-Group"]
     },
   ]
 
